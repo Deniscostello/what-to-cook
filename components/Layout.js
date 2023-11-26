856e5b9e5d29f5ef67b07f372d1a0590a4b8a5b4
Denis Costello <G00396309@atu.ie>
2023-11-26 22:18:05 +0000

    Added testLayout for the main Layout

--- components/Layout.js
+++ components/Layout.js
@@ -1,13 +1,26 @@
 import React from 'react'
 import classes from './Layout.module.css'
 import Navbar from './navbar/Navbar';
+import { Fragment } from 'react';
 
 function Layout() {
   return (
-    <div className={classes.mainDiv}>
-    <Navbar />
-    </div>
-    
+    <>
+      <div>
+        <Navbar />
+      </div>
+
+      <div className={classes.mainDiv}>
+        <div className={classes.layout}>Hello World
+          <div className={classes.carouselDiv}>Easy and convenient meals based on food you already have</div>
+          <div className={classes.recipeDiv}>
+          <div >Dinner Ideas</div>
+          <div>Light Bites</div>
+          </div>
+        </div>
+
+      </div>
+    </>
   )
 }
 
