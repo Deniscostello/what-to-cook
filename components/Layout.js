91eb09c1d42457cb82a472d269fbb900593ae602
Denis Costello <G00396309@atu.ie>
2023-11-26 16:47:26 +0000

    First Commit

---
+++ components/Layout.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import classes from './Layout.module.css'
+function Layout() {
+  return (
+    <div className={classes.mainDiv}>Layout</div>
+  )
+}
+
+export default Layout;
