70badbfbff8e2ab652200b492a753bb177275364
Denis Costello <G00396309@atu.ie>
2023-12-03 13:42:34 +0000

    Hamburger-Menu-Working

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,12 @@
 import '@/styles/globals.css'
+import {GlobalContextProvider} from './store/globalContext'
+import Layout from '@/components/Layout';
+
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <GlobalContextProvider>
+      <Component {...pageProps} />
+    </GlobalContextProvider>
+  );
 }
--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,24 @@
 import Head from 'next/head'
 import styles from '@/styles/Home.module.css'
 import Layout from '@/components/Layout'
+import GlobalContext from './store/globalContext'
+import { useContext } from 'react'
 
 export default function Home() {
-  return (
-    <>
-      <Head>
-        <title>What To Cook App</title>
-        <meta name="description" content="What To Cook Project" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-      </Head>
-      <main className={`${styles.main} `}>
-        <Layout />
-      </main>
-    </>
-  )
+  const globalCtx = useContext(GlobalContext)
+  // if (globalCtx.theGlobalObject.dataLoaded == true) {
+    return (
+      <>
+        <Head>
+          <title>What To Cook App</title>
+          <meta name="description" content="What To Cook Project" />
+          <meta name="viewport" content="width=device-width, initial-scale=1" />
+        </Head>
+        <main className={`${styles.main} `}>
+          <Layout />
+        </main>
+      </>
+    )  
+
+
 }
