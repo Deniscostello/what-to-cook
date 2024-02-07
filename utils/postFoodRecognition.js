a03db99b31199d6a22b85632b10b1df646983ef3
Denis Costello <G00396309@atu.ie>
2024-02-07 20:10:38 +0000

    Take snapshot to add into FormData

---
+++ utils/postFoodRecognition.js
@@ -0,0 +1,15 @@
+import { FoodRecognitionResponse } from "@/types";
+
+export async function postFoodRecognition(snapshot) {
+  const formdata = new FormData();
+  formdata.set('snapshot', snapshot);
+
+  const request = {
+    method: 'POST',
+    body: formdata,
+  };
+
+  const resp = await fetch('/api/food-recognition', request);
+  const json = await resp.json();
+  return json;
+}
