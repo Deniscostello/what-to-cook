e13494a7f8b1e3cff91f4821e41a7f7dbc557db4
Denis Costello <G00396309@atu.ie>
2024-02-07 20:04:59 +0000

    Clarifai recognizes or not, return name and score

--- pages/api/food-recognition.js
+++ pages/api/food-recognition.js
@@ -1,1 +1,15 @@
+import { FoodRecognitionResponse } from '@/types';
+import { NextApiRequest, NextApiResponse } from 'next'
 
+export default function handler(req,res) {
+  if (req.method !== 'POST') {
+    console.warn(`Method ${req.method} not allowed for endpoint /food-recognition!`);
+    return res.status(405).end();
+  }
+  const score = Math.random();
+  const response = score > 0.4
+    ? { name: 'John Doe', score, recognized: true }
+    : { recognized: false };
+
+  res.status(200).json(response);
+}
--- types.js
+++ types.js
@@ -2,10 +2,10 @@
  * If Clarifai can match us with a celebrity, return the name and score.
  * Otherwise, just return `false`.
  */
-export type FoodRecognitionResponse = {
-    recognized: true;
-    name: string;
-    score: number;
+const FoodRecognitionResponse = {
+    recognized: true,
+    name: string,
+    score: number,
   } | {
-    recognized: false;
+    recognized: false
   }
